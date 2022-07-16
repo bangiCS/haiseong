d9c03992ec3fafd6c4b591baf54d562c990c3252
haiseong <wjdgotjd9908@gmail.com>
2022-07-16 21:19:12 +0900

    6064

--- hw1/6064.py
+++ hw1/6064.py
@@ -1,21 +1,18 @@
+import sys
+input = sys.stdin.readline
 
-time = int(input())
+def calculate(m, n, x, y):
+    k = x
+    while k <= m * n:
+        if (k - y) % n == 0:
+            return k
+        k += m
+    return -1
 
-for _ in range(time):
-    m,n,x,y = map(int,input().split())
-    i = x
-    j = y
-    cnt = 1
-    while True:
-        cnt+=1
-        i = (i + m-1)%m
-        j = (j + n-1)%n
-        if i==1 and j==1:
-            break
-        if cnt > m*n:
-            break
 
-    if i==1 and j==1:
-        print(cnt)
-    else:
-        print(-1)
+t = int(input())
+
+for _ in range(t):
+    m, n, x, y = map(int, input().split())
+
+    print(calculate(m, n, x, y))

d949168423794abe953bacdf25e8a03a2c217b52
haiseong <wjdgotjd9908@gmail.com>
2022-07-27 15:25:41 +0900

    hw3 4

--- hw3/16194_timeout.py
+++ hw3/16194_timeout.py
@@ -3,11 +3,6 @@ import math
 n = int(input())
 cards = [0] + list(map(int, input().split()))
 
-# for i in range(1,len(cards)):
-#     for j in range(i//2+1):
-#         cards[i] = min(cards[i], cards[i-j-1] + cards[j])
-#
-# print(cards[-1])
 answer = 0
 
 while n > 0:
@@ -17,8 +12,6 @@ while n > 0:
         if min_sum > math.ceil(n/i) * cards[i]:
             min_sum = math.ceil(n/i) * cards[i]
             min_idx = i
-    # while n >= min_idx:
-    #     n -= min_idx
     n = n%min_idx
     answer += min_sum
 
--- hw4/16948.py
+++ hw4/16948.py
@@ -1,14 +1,26 @@
+from collections import deque
 
 n = int(input())
-dp = [[0] * 10 for _ in range(n+1)] # dp[y][x] : x로 끝나는 y자리 수의 갯수
+r1,c1,r2,c2 = map(int,input().split())
+graph = [[-1]*n for _ in range(n)]
 
-for i in range(1,9+1):
-    dp[1][i] = 1
+queue = deque([(r1,c1)])
+graph[r1][c1] = 0
+is_solved = False
 
-for i in range(2,n+1):
-    dp[i][0] = dp[i-1][1] % 1000000000
-    for j in range(1,9):
-        dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % 1000000000
-    dp[i][9] = dp[i-1][8] % 1000000000
+while queue:
+    r, c = queue.popleft()
 
-print(sum(dp[-1]) % 1000000000)
+    if r == r2 and c == c2:
+        print(graph[r][c])
+        is_solved = True
+        break
+
+    for next_r, next_c in [(r-2, c-1), (r-2, c+1), (r, c-2), (r, c+2), (r+2, c-1), (r+2, c+1)]:
+        if 0<=next_r<n and 0<=next_c<n:
+            if graph[next_r][next_c] == -1:
+                queue.append((next_r,next_c))
+                graph[next_r][next_c] = graph[r][c] + 1
+
+if not is_solved:
+    print(-1)
